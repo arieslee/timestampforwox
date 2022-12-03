d87382a905550bbda0e16f53d2cf19dfd3cd60e6
kongtianyi <kongtianyi@foxmail.com>
2022-12-03 19:15:23 +0800

    feat: add timezone feature

--- transform.py
+++ transform.py
@@ -9,9 +9,10 @@ import os
 import time
 import copy
 from wox import Wox, WoxAPI
+from datetime import datetime, timedelta, timezone
 result_template = {
-    'Title': '{}:{}',
-    'SubTitle': '点击复制到剪贴板',
+    'Title': '{}: {}',
+    'SubTitle': 'copy to clipboard',
     'IcoPath': 'ui/icon.png',
     'JsonRPCAction': {
         'method': 'copy_to_clipboard',
@@ -25,30 +26,53 @@ class Main(Wox):
     def query(self, key):
         result = []
         param = key.strip()
-        if param == 'now':
-            param = int(time.time())
-            local_time = time.localtime(param)
-            time_result = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+        query_time = param
+
+        aimed_timezone_offset = 0
+        param_split = param.split(' ', 1)
+        is_aimed_timezone = len(param_split) == 2 and param_split[0].startswith('utc')
+        if is_aimed_timezone:
+            aimed_timezone_str = param_split[0]
+            if len(aimed_timezone_str) > 3:
+                aimed_timezone_offset = int(aimed_timezone_str[3:])
+            query_time = param_split[1]
+
+        if query_time == 'now' or (is_aimed_timezone and param_split[1] == 'now'):
+            local_time = datetime.now()
+            query_time = int(local_time.timestamp())
+            time_result = self.format_timestamp(query_time, is_aimed_timezone, aimed_timezone_offset)
+
             unix_format = copy.deepcopy(result_template)
-            unix_format['Title'] = unix_format['Title'].format('当前时间：', param)
-            unix_format['JsonRPCAction']['parameters'][0] = unix_format['JsonRPCAction']['parameters'][0].format(param)
+            unix_format['Title'] = unix_format['Title'].format('now', query_time)
+            unix_format['JsonRPCAction']['parameters'][0] = unix_format['JsonRPCAction']['parameters'][0].format(query_time)
             result.append(unix_format)
         else:
-            unix_time = self.is_unixtime(param)
+            unix_time = self.is_unixtime(query_time)
             if unix_time is False:
-                if ":" in param:
-                    time_data = time.strptime(param, "%Y-%m-%d %H:%M:%S")
+                if ":" in query_time:
+                    time_data = datetime.strptime(query_time, "%Y-%m-%d %H:%M:%S")
                 else:
-                    time_data = time.strptime(param, "%Y-%m-%d")
-                time_result = int(time.mktime(time_data))
+                    time_data = datetime.strptime(query_time, "%Y-%m-%d")
+                if is_aimed_timezone:
+                    time_data = time_data.replace(tzinfo=timezone(timedelta(hours=aimed_timezone_offset)))
+                    time_result = time_data.timestamp()
+                else:
+                    time_result = time_data.timestamp()
             else:
-                local_time = time.localtime(unix_time)
-                time_result = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+                time_result = self.format_timestamp(unix_time, is_aimed_timezone, aimed_timezone_offset)
 
-        result_template['Title'] = '转换结果：%s' % (time_result,)
+        result_template['Title'] = 'result: %s' % (time_result,)
         result_template['JsonRPCAction']['parameters'][0] = time_result
         result.append(result_template)
         return result
+
+    def format_timestamp(self, timestamp, is_aimed_timezone, aimed_timezone_offset):
+        tz_aimed = timezone(timedelta(hours=aimed_timezone_offset))
+        if is_aimed_timezone:
+            time_result = datetime.fromtimestamp(timestamp, tz_aimed).strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            time_result = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S")
+        return time_result
 
     def copy_to_clipboard(self, value):
         command = 'echo ' + str(value).strip() + '| clip'
@@ -57,9 +81,9 @@ class Main(Wox):
     def is_unixtime(self, value):
         try:
             strlen = len(value)
-            '''支持10位或者13位unixtime'''
+            '''support 10 or 13 length unixtime |支持10位或者13位unixtime'''
             if strlen != 10 and strlen != 13:
-                raise Exception('时间格式错误')
+                raise Exception('timestamp format error')
             data = int(value)
             if strlen == 13:
                 data = data / 1000.0
